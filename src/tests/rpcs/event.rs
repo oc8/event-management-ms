e1cb3860b58ad90a82d5929f2d65db9c22c40bb3
X3ne <X3ne@users.noreply.github.com>
2024-05-21 08:50:49 -0400

    test: add some create event tests

--- src/tests/rpcs/event.rs
+++ src/tests/rpcs/event.rs
@@ -8,7 +8,7 @@ use protos::booking::v1::{CreateEventRequest, EventType, GetEventRequest};
 
 #[tokio::test]
 async fn create_basic_event() -> Result<(), Box<dyn std::error::Error>> {
-    let ctx = TestContext::new("postgres://postgres:postgres@localhost:5433", "create_basic_event", "redis://:@localhost:6382", 6070);
+    let ctx = TestContext::new("postgres://postgres:postgres@localhost:5433", "create_basic_event", "redis://:@localhost:6382", 6100);
     let (tx, rx) = oneshot::channel();
     let service = ctx.service.clone();
 
@@ -45,7 +45,7 @@ async fn create_basic_event() -> Result<(), Box<dyn std::error::Error>> {
 
 #[tokio::test]
 async fn get_event_not_found() -> Result<(), Box<dyn std::error::Error>> {
-    let ctx = TestContext::new("postgres://postgres:postgres@localhost:5433", "get_event_not_found", "redis://:@localhost:6382", 6071);
+    let ctx = TestContext::new("postgres://postgres:postgres@localhost:5433", "get_event_not_found", "redis://:@localhost:6382", 50300);
     let (tx, rx) = oneshot::channel();
     let service = ctx.service.clone();
 
@@ -80,7 +80,7 @@ async fn get_event_not_found() -> Result<(), Box<dyn std::error::Error>> {
 
 #[tokio::test]
 async fn get_event() -> Result<(), Box<dyn std::error::Error>> {
-    let ctx = TestContext::new("postgres://postgres:postgres@localhost:5433", "get_event", "redis://:@localhost:6382", 6072);
+    let ctx = TestContext::new("postgres://postgres:postgres@localhost:5433", "get_event", "redis://:@localhost:6382", 50301);
     let (tx, rx) = oneshot::channel();
     let service = ctx.service.clone();
 
@@ -118,6 +118,80 @@ async fn get_event() -> Result<(), Box<dyn std::error::Error>> {
     let resp = client.get_event(request).await?;
 
     assert_eq!(event.id, resp.into_inner().event.unwrap().id);
+
+    tx.send(()).unwrap();
+    jh.await.unwrap();
+    Ok(())
+}
+
+#[tokio::test]
+async fn create_recurrent_event() -> Result<(), Box<dyn std::error::Error>> {
+    let ctx = TestContext::new("postgres://postgres:postgres@localhost:5433", "create_recurrent_event", "redis://:@localhost:6382", 50302);
+    let (tx, rx) = oneshot::channel();
+    let service = ctx.service.clone();
+
+    let jh = tokio::spawn(async move {
+        Server::builder()
+            .add_service(BookingServiceServer::new(service))
+            .serve_with_shutdown(ctx.addr, rx.map(|_| ()))
+            .await
+            .unwrap();
+    });
+
+    tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+
+    let mut client = BookingServiceClient::connect(ctx.url.clone()).await.unwrap();
+
+    let request = tonic::Request::new(CreateEventRequest {
+        name: "test-event".to_string(),
+        start: "2024-05-26T09:00:00".to_string(),
+        end: "2024-05-26T12:00:00".to_string(),
+        timezone: "Europe/Paris".to_string(),
+        organizer_key: "test-organizer".to_string(),
+        slot_duration: 0,
+        capacity: 0,
+        slot_capacity: 0,
+        recurrence_rule: "FREQ=WEEKLY;BYDAY=SU".to_string(),
+        event_type: EventType::Event as i32,
+    });
+    client.create_event(request).await?;
+
+    tx.send(()).unwrap();
+    jh.await.unwrap();
+    Ok(())
+}
+
+#[tokio::test]
+async fn create_meeting_event() -> Result<(), Box<dyn std::error::Error>> {
+    let ctx = TestContext::new("postgres://postgres:postgres@localhost:5433", "create_meeting_event", "redis://:@localhost:6382", 50302);
+    let (tx, rx) = oneshot::channel();
+    let service = ctx.service.clone();
+
+    let jh = tokio::spawn(async move {
+        Server::builder()
+            .add_service(BookingServiceServer::new(service))
+            .serve_with_shutdown(ctx.addr, rx.map(|_| ()))
+            .await
+            .unwrap();
+    });
+
+    tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+
+    let mut client = BookingServiceClient::connect(ctx.url.clone()).await.unwrap();
+
+    let request = tonic::Request::new(CreateEventRequest {
+        name: "test-event".to_string(),
+        start: "2024-05-26T09:00:00".to_string(),
+        end: "2024-05-26T12:00:00".to_string(),
+        timezone: "Europe/Paris".to_string(),
+        organizer_key: "test-organizer".to_string(),
+        slot_duration: 15,
+        capacity: 15,
+        slot_capacity: 0,
+        recurrence_rule: "FREQ=WEEKLY;BYDAY=SU".to_string(),
+        event_type: EventType::Event as i32,
+    });
+    client.create_event(request).await?;
 
     tx.send(()).unwrap();
     jh.await.unwrap();
